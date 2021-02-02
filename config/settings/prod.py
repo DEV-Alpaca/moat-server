f41a314fe720bc2c03ff3b5b3157670306263d32
Jinho Lee <49632119+justzino@users.noreply.github.com>
2021-02-02 20:46:56 +0900

    FEAT(settings): config/settings/prod.py 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,1 +1,15 @@
 from .base import *  # noqa
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, "config/.env.prod"))
+
+DATABASES = {
+    "default": {
+        "ENGINE": env("POSTGRES_ENGINE"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
+    }
+}
