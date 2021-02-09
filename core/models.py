c403b0b2b6664c0363c15f187dfecd56ceb1c94f
Jinho Lee <49632119+justzino@users.noreply.github.com>
2021-02-09 23:10:43 +0900

    FEAT: Add Core Model for Time Stamp

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class CoreModel(models.Model):
+
+    """ Time Stamped Model """
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
