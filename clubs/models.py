a1a238f4d9f75e5d2ec5de8eb256200a70f5115e
Jinho Lee <49632119+justzino@users.noreply.github.com>
2021-02-09 23:15:40 +0900

    FEAT: Add Club model and Photo model in clubs/models.py

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+from core.models import CoreModel
+
+
+class Club(CoreModel):
+
+    """ Club Model Definition """
+
+    CLUB_SMALL_GROUP = "SG"
+    CLUB_TALENT_SHARE = "TS"
+
+    CLUB_TYPE_CHOICES = ((CLUB_SMALL_GROUP, "소모임"), (CLUB_TALENT_SHARE, "재능공유"))
+
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    address = models.CharField(max_length=140)
+    club_type = models.CharField(choices=CLUB_TYPE_CHOICES, max_length=2)
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="rooms"
+    )
+
+    def __str__(self):
+        return self.name
+
+    def photo_number(self):
+        return self.photos.count()
+
+    photo_number.short_description = "Photo Count"
+
+    class Meta:
+        ordering = ["-pk"]
+
+
+class Photo(CoreModel):
+
+    """ Photo Model Definition """
+
+    file = models.ImageField()
+    club = models.ForeignKey("Club", related_name="photos", on_delete=models.CASCADE)
+    caption = models.CharField(max_length=140)
+
+    def __str__(self):
+        return self.club.name
