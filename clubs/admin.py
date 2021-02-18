7e79ae908f221287a44017e9b3d440ec0160f9f7
Jinho Lee <49632119+justzino@users.noreply.github.com>
2021-02-18 20:33:39 +0900

    FEAT: Make Club Admin with get_thumbnail Method

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,17 +1,53 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 
 from . import models
+
+
+class PhotoInline(admin.TabularInline):
+
+    model = models.Photo
 
 
 @admin.register(models.Club)
 class ClubAdmin(admin.ModelAdmin):
 
+    """ Club Admin Definition """
+
+    inlines = (PhotoInline,)
+
     list_display = (
         "name",
-        "photo_number",
+        "d_date",
+        "host",
+        "fav_count",
+        "applicant_count",
+        "photo_count",
+        "address",
     )
+
+    ordering = ("d_date",)
 
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    pass
+
+    """ Photo Admin Definition """
+
+    list_display = ("__str__", "get_thumbnail")
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img width="50px" src="{obj.file.url}" />')
+
+    get_thumbnail.short_description = "Thumbnail"
+
+
+@admin.register(models.ClubType, models.Address)
+class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.clubs.count()
