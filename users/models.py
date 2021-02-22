9808feaecc89da9b65fa7e2cc445f6e173435fb2
Jinho Lee <49632119+justzino@users.noreply.github.com>
2021-02-22 20:31:34 +0900

    FEAT: Add some fields in User Model

--- users/models.py
+++ users/models.py
@@ -7,7 +7,19 @@ class User(AbstractUser):
 
     """ User Model Definition """
 
-    avatar = models.ImageField(upload_to="avatars", blank=True)
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "male"),
+        (GENDER_FEMALE, "female"),
+        (GENDER_OTHER, "other"),
+    )
+
     name = models.CharField(_("name"), max_length=150, blank=True)
     superhost = models.BooleanField(default=False)
+    avatar = models.ImageField(upload_to="avatars", blank=True)
+    birthday = models.DateField(blank=True, null=True)
     mobile = models.CharField(verbose_name="휴대폰 번호", primary_key=True, max_length=11)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
